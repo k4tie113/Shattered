daea2a6b2e91488e8305350adfc2205231d921e3
Katie Li <katieli167@gmail.com>
2023-08-01 10:06:33 -0700

    Improved interaction (not done yet though)

--- Final Project Prototype/Assets/FurnishedCabin/Scripts/Interactable.cs
+++ Final Project Prototype/Assets/FurnishedCabin/Scripts/Interactable.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
     public float radius = 3f;
+    public float msgTimer = 3f;
     public bool hasInteracted;
     public bool isInteracting;
+    public bool canDisplayText;
     public bool tooFar;
     public string displayMsg;
     private GUIStyle textStyle;
@@ -16,7 +18,8 @@ public class Interactable : MonoBehaviour
     {
         tooFar = false;
         hasInteracted = false;
-        displayMsg = "";
+        canDisplayText = false;
+        displayMsg = "Interacting";
         textStyle = new GUIStyle();
 		textStyle.fontSize = 16;
 		textStyle.fontStyle = FontStyle.Italic;
@@ -25,15 +28,23 @@ public class Interactable : MonoBehaviour
     public void hit(Transform playerPOS)
     {
         player = playerPOS;
+        tooFar = false;
         float distance = Vector3.Distance(player.position, transform.position);
-        if(distance<radius)
+        if(!canDisplayText) canDisplayText = true;
+        Debug.Log(canDisplayText);
+        if(distance>radius)
         {
-            tooFar = false;
+            Invoke("TextCooldown", 1f);
+            tooFar = true;
+        }
+        else
+        {
+            Invoke("TextCooldown", msgTimer);
             Interact();
             isInteracting = true;
             hasInteracted = true;
         }
-        tooFar = true;
+
     }
     public virtual void Interact()
     {
@@ -42,9 +53,18 @@ public class Interactable : MonoBehaviour
 
     void OnGUI()
     {
+        if(!canDisplayText) return;
         if(tooFar)
         {
             GUI.Label(new Rect (50,Screen.height - 50,200,50), "walk closer to interact",textStyle);
         }
+        else
+        {
+            GUI.Label(new Rect (50,Screen.height - 50,200,50), displayMsg,textStyle);
+        }
+    }
+    void TextCooldown()
+    {
+        canDisplayText = false;
     }
 }
