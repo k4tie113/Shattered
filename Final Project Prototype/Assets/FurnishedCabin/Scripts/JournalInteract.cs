3ffbc82c5fe38eeef9c95772798908df74c34b8f
Katie Li <katieli167@gmail.com>
2023-08-02 12:17:25 -0700

    added other photo

--- Final Project Prototype/Assets/FurnishedCabin/Scripts/JournalInteract.cs
+++ Final Project Prototype/Assets/FurnishedCabin/Scripts/JournalInteract.cs
@@ -16,12 +16,7 @@ public class JournalInteract : Interactable
         index = 0;
       guiStyle = FontManager.guiStyle;
       noteCanvas.SetActive(false);
-   }
-   public override void Interact()
-   {
-        base.Interact();
-        
-        texts[0] = "Patricia's Art Journal. Don't mess it up - put inside my drawer if you find it";
+       texts[0] = "Patricia's Art Journal. Don't mess it up - put inside my drawer if you find it";
         texts[1] = "August 15, 1997\nIt's been a year since I've written a journal entry. It's already been a year since that day. I can't believe it. I don't think my kids will find this. My job has always been uninteresting to them.";
         texts[2] = "October 9, 1997\nThe doctor says that she has dissociative amnesia. She can't remember anything. Paul might not think so, but I think it's a good thing.";
         texts[3] = "November 25, 1997\nIt's been a month since I've hidden all the family photos. The walls feel so empty. I miss my daughter.";
@@ -29,12 +24,16 @@ public class JournalInteract : Interactable
         texts[5] = "May 10, 1998\nI've been having some nightmares lately about our home. It's nothing to worry about.";
         texts[6] = "May 11, 1998\nI saw her in the mirror today. I wanted to tell her that I miss her. I wanted to tell her that I'm so sorry for what I did to her. I don't deserve to be called a mother. I'm a monster. Help me. She's going to take me with her.";
         texts[7] = "I\'m seeing her face in the windows as well. God help me.";
+   }
+   public override void Interact()
+   {
+        index = 0;
+        base.Interact();
         noteText.text = texts[index];
         ShowNote();
    }
    public void ShowNote()
    {
-        index = 0;
       isOpen = true;
       GUIManager.canMove = false;
       noteCanvas.SetActive(true);
