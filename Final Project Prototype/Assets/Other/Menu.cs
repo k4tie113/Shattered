ecca2cc936fbe94456af8b79097e1dbf6f9bf266
namgasilver <141055951+namgasilver@users.noreply.github.com>
2023-08-02 22:12:04 -0700

    made the instructions page

--- Final Project Prototype/Assets/Other/Menu.cs
+++ Final Project Prototype/Assets/Other/Menu.cs
@@ -7,7 +7,7 @@ public class Menu : MonoBehaviour
 {
    public void OnPlayButton()
    {
-        SceneManager.LoadScene("Demo");
+        SceneManager.LoadScene("Instructions");
    }
 
    public void OnQuitButton()
