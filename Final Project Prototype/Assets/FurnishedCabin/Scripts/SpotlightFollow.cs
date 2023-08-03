face8dd9a92b6cdadd6063500b2c162b5a359760
Katie Li <katieli167@gmail.com>
2023-08-02 19:26:04 -0700

    added spotlight

---
+++ Final Project Prototype/Assets/FurnishedCabin/Scripts/SpotlightFollow.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightFollow : MonoBehaviour
+{
+    public GameObject flashlight;
+
+    //public AudioSource turnOn;
+    //public AudioSource turnOff;
+
+    public bool state;
+    GUIStyle style;
+
+
+
+    void Start()
+    {
+        style = FontManager.guiStyle;
+        state = false;
+        flashlight.SetActive(state);
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect (Screen.width * 0.7f,Screen.height*0.1f,200,50), "[F] to toggle Flashlight",style);
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            state = !state;
+            flashlight.SetActive(state);
+            
+        }
+        
+    }
+}
